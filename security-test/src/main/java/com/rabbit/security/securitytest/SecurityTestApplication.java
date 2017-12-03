0f9db86e6e49f2826776c6a18f86b4612801d8b6
guoyankui <yankuiguo@gmail.com>
2017-12-03 17:55:17 +0800

    init a project

---
+++ security-test/src/main/java/com/rabbit/security/securitytest/SecurityTestApplication.java
@@ -0,0 +1,12 @@
+package com.rabbit.security.securitytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTestApplication.class, args);
+	}
+}
